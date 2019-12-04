73eeab1861b69762a5d6b7b8ad31df2a2caaf0ac
Davis Mohar <davismohar@gmail.com>
2019-12-04 23:20:45 +0000

    final project

--- contract/project3/README.txt
+++
@@ -1,4 +0,0 @@
- --- project3 Project ---
-
- - How to Build -
-   - run the command 'eosio-cpp -abigen -o project3.wasm project3.cpp'
--- contract/project3/project3.cpp
+++
@@ -1,5 +0,0 @@
-#include "project3.hpp"
-ACTION project3::hi( name nm ) {
-   /* fill in action body */
-   print_f("Name : %\n",nm);
-}
